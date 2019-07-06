5dc23d2988059068781eb43f5e98111eb7dc55cb
Oleg Shvedov <ol.shvedov@gmail.com>
2019-07-07 00:34:45 +0300

    Fix header adaptive

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import classNames from 'classnames';
 
+import sizeContext from 'context/size-context';
 import { ReactComponent as Logo } from './logo.svg';
-import './header.module.scss';
+import classes from './header.module.scss';
 
 export interface HeaderProps {
   children?: React.ReactNode;
 }
 
-export const Header: React.FC<HeaderProps> = props => (
-  <header>
-    <Logo />
-    {props.children}
-  </header>
-);
+export const Header: React.FC<HeaderProps> = props => {
+  const size = useContext(sizeContext);
+
+  return (
+    <header
+      className={classNames(classes.root, {
+        [classes.sm]: size === 'large',
+      })}
+    >
+      <Logo />
+      {props.children}
+    </header>
+  );
+};
