b08623ad0487f2bbd7ac6b882cd7f215706c1b7b
Oleg Shvedov <ol.shvedov@gmail.com>
2019-09-02 18:52:01 +0300

    Get rid of unused local resolvers

--- src/service/resolvers.ts
+++ src/service/resolvers.ts
@@ -1,6 +1,6 @@
 import { Resolvers } from 'apollo-client';
 
-import { FLOORS_QUERY, TABLE_QUERY } from './queries';
+import { TABLE_QUERY } from './queries';
 
 export const resolvers: Resolvers = {
   Query: {
@@ -11,20 +11,6 @@ export const resolvers: Resolvers = {
       if (cacheData.table && cacheData.table.has(timestamp)) {
         return cacheData.table.get(timestamp)[id];
       }
-    },
-  },
-  Mutation: {
-    writeFloors(_root, { floors }, { cache }) {
-      cache.writeQuery({
-        query: FLOORS_QUERY,
-        data: { floors },
-      });
-    },
-    writeTable(_root, { table }, { cache }) {
-      cache.writeQuery({
-        query: TABLE_QUERY,
-        data: { table },
-      });
     },
   },
 };
