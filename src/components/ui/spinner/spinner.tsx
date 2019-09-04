2e297da91be81b970b4afb93922a5ea722353230
Oleg Shvedov <ol.shvedov@gmail.com>
2019-09-04 12:25:10 +0300

    Add support for transparent background in spinner

--- src/components/ui/spinner/spinner.tsx
+++ src/components/ui/spinner/spinner.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import cn from 'classnames';
 
 import classes from './spinner.module.scss';
 
-export const Spinner = () => (
-  <div className={classes.loader}>
+type Props = {
+  transparent?: boolean;
+};
+
+export const Spinner = ({ transparent = false }: Props) => (
+  <div className={cn(classes.loader, { [classes.transparent]: transparent })}>
     <div className="vertical-helper" />
     <div className="spinner" />
   </div>
