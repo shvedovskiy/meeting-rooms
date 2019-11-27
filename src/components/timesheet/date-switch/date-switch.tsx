34a6c8ebf218cab9d7326edff64f76cd186ddadf
Oleg Shvedov <ol.shvedov@gmail.com>
2019-11-27 14:41:39 +0300

    Move outside clicks handling into calendar component

--- src/components/timesheet/date-switch/date-switch.tsx
+++ src/components/timesheet/date-switch/date-switch.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useState, useEffect, createRef } from 'react';
+import React, { useCallback, useState } from 'react';
 import cn from 'classnames';
-import DayPicker from 'react-day-picker';
 import { format, isToday, addDays, subDays } from 'date-fns/esm';
 import ruLocale from 'date-fns/locale/ru';
 
@@ -17,22 +16,8 @@ type Props = {
 };
 
 export const DateSwitch = (props: Props) => {
-  const calendarRef = createRef<DayPicker>();
   const { date, size = 'default', onChange } = props;
   const [calendarVisible, setCalendarVisible] = useState(false);
-
-  useEffect(() => {
-    function checkOutsideClick(event: MouseEvent) {
-      if (
-        calendarRef.current &&
-        !calendarRef.current.dayPicker.contains(event.target as Element)
-      ) {
-        setCalendarVisible(false);
-      }
-    }
-    window.addEventListener('click', checkOutsideClick);
-    return () => window.removeEventListener('click', checkOutsideClick);
-  }, [calendarRef]);
 
   const handleDayChange = useCallback(
     (nextDirection: boolean) => {
@@ -101,7 +86,6 @@ export const DateSwitch = (props: Props) => {
             initialDate={date}
             onBlur={() => setCalendarVisible(false)}
             onChange={handleDateSelect}
-            ref={calendarRef}
           />
         )}
       </div>
--- src/components/ui/calendar/calendar.tsx
+++ src/components/ui/calendar/calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, forwardRef, FocusEventHandler } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import { startOfDay } from 'date-fns/esm';
 
@@ -9,14 +9,28 @@ import classes from './calendar.module.scss';
 type Props = {
   className?: string;
   initialDate?: Date;
-  onBlur?: FocusEventHandler;
+  onBlur: () => void;
   onChange: (newDate: Date) => void;
 };
 
-export const Calendar = forwardRef<DayPicker, Props>((props, outerRef) => {
+export const Calendar = (props: Props) => {
   const now = new Date();
   const { className, initialDate = now, onBlur, onChange } = props;
   const [selected, setSelected] = useState(initialDate);
+  const pickerRef = useRef<DayPicker>(null);
+
+  useEffect(() => {
+    function checkOutsideClick(event: MouseEvent) {
+      if (
+        pickerRef.current &&
+        !pickerRef.current.dayPicker.contains(event.target as Element)
+      ) {
+        onBlur();
+      }
+    }
+    window.addEventListener('click', checkOutsideClick);
+    return () => window.removeEventListener('click', checkOutsideClick);
+  });
 
   function handleDayClick(date: Date, modifiers: DayModifiers) {
     if (!modifiers.disabled && !modifiers.selected) {
@@ -41,11 +55,11 @@ export const Calendar = forwardRef<DayPicker, Props>((props, outerRef) => {
       }}
       month={selected}
       months={MONTHS}
-      ref={outerRef}
+      ref={pickerRef}
       onBlur={onBlur}
       onDayClick={handleDayClick}
       showOutsideDays
       weekdayElement={Weekday}
     />
   );
-});
+};
