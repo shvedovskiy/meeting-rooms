d9901d5cd622d2e772bc278630fa53fe3f7b5660
Oleg Shvedov <ol.shvedov@gmail.com>
2019-12-07 18:44:31 +0300

    Update create react app

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,15 +1,1 @@
-import { configure } from 'enzyme';
-// import { configure, shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-// import toJson from 'enzyme-to-json';
-
-configure({ adapter: new Adapter() });
-
-// global.shallow = shallow;
-// global.render = render;
-// global.mount = mount;
-// global.toJson = toJson;
-
-console.error = (message: string) => {
-  throw new Error(message);
-};
+import '@testing-library/jest-dom/extend-expect';
