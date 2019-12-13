6e8143c9d0d89636b3976e883bc7c80b56b2fa97
Oleg Shvedov <ol.shvedov@gmail.com>
2019-12-14 02:23:17 +0300

    Add maxAge header to static assets

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,8 @@ import path from 'path';
 import { useContainer } from 'typeorm';
 import { Container } from 'typedi';
 import express from 'express';
-import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
+import favicon from 'serve-favicon';
 
 import { HOST, PORT, NODE_ENV, FRONTEND_URL } from './service/config';
 import { createSchema } from './service/create-schema';
@@ -20,17 +20,26 @@ async function bootstrapServer() {
     const server = new ApolloServer({ schema, context: {} });
 
     const app = express();
-    app.use(express.static(path.join(__dirname, 'assets')));
-    app.use(express.static(path.join(__dirname, '..', 'build')));
-    if (FRONTEND_URL.trim().length) {
-      app.use(
-        cors({
-          origin: FRONTEND_URL,
-          credentials: true,
-        })
-      );
+    if (NODE_ENV === 'production') {
+      app.use(favicon(path.join(__dirname, '..', 'build', 'favicon.ico')));
     }
-    server.applyMiddleware({ app, cors: false });
+    app.use(
+      '/static',
+      express.static(path.join(__dirname, '..', 'build', 'static'), {
+        maxAge: 31536000,
+      })
+    );
+    app.use(express.static(path.join(__dirname, '..', 'build')));
+    app.use(express.static(path.join(__dirname, 'assets')));
+    server.applyMiddleware({
+      app,
+      cors: FRONTEND_URL
+        ? {
+            origin: FRONTEND_URL,
+            credentials: true,
+          }
+        : false,
+    });
 
     app.listen({ port: PORT }, (err?: string) => {
       if (err) {
