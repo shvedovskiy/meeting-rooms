5da21999a5b618982640ac1504dac2e422e88e33
Oleg Shvedov <ol.shvedov@gmail.com>
2019-12-20 15:15:19 +0300

    Add display safe area

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 
 import { ReactComponent as Logo } from './logo.svg';
 import classes from './header.module.scss';
+import { useSizeCtx } from 'context/size-context';
 
 export interface HeaderProps {
   children?: React.ReactNode;
 }
 
 export const Header = ({ children }: HeaderProps) => {
+  const size = useSizeCtx();
+
   return (
-    <header className={classes.root}>
+    <header className={cn(classes.root, { [classes.lg]: size === 'large' })}>
       <Logo />
       {children}
     </header>
